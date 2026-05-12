85f1fdccd5c0a8e7332d60096293ded859bf8934
Sunny | Engineering Student | Computer Science Enthusiast <137380955+Sunny-Saurya@users.noreply.github.com>
2026-05-12 14:36:01 +0530

    Update db.js

--- Server/config/db.js
+++ Server/config/db.js
@@ -4,9 +4,11 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {});
         console.log("MongoDB connected successfully");
+        console.log(process.env.MONGODB_URI);
 
     }
     catch (err) {
+        console.log(process.env.MONGODB_URI);
         console.error("Database connection failed:", err);
         process.exit(1);
     }
